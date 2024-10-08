109f11559b5b0f86e701e11049e46bd33247f509
Armando Rosales <refill.1509@gmail.com>
2024-10-08 00:05:31 -0600

    requirements.txt

--- sql/databases/biblioteca.py
+++ sql/databases/biblioteca.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import contains_eager, sessionmaker
 
-connection_string = "mssql+pyodbc://sa:yourStrong#Password@localhost/biblioteca?driver=ODBC+Driver+17+for+SQL+Server"
-
+# connection_string = "po+pyodbc://sa:yourStrong#Password@localhost/biblioteca?driver=ODBC+Driver+17+for+SQL+Server"
+connection_string = "postgresql+psycopg2://postgres:Rosmacias1500@reichikita.zapto.org:5432/biblioteca"
 # Crear el motor de conexión
 engine = create_engine(connection_string)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
